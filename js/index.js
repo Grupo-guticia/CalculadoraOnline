562e2245f01ac2d23a3731676aaf2988b2b90f4c
crisariasgg <crisarias@grupoguticia.com>
2021-07-26 12:33:01 -0600

    added index.js and mapped html elements

---
+++ js/index.js
@@ -0,0 +1,30 @@
+(function () {
+
+    //
+	// Variables
+	//
+    const peso = document.getElementById("peso_libras");
+    const valor_compra = document.getElementById("valor_compra");
+    const producto = document.getElementById("producto");
+    const delivery = document.getElementsByTagName("radioDelivery");
+
+    const flete = document.getElementById("")
+    const tramite_aduanal = document.getElementById("")
+    const impuestos = document.getElementById("")
+    const cepa = document.getElementById("")
+    const total_cargos_importacion = document.getElementById("")
+    const comisión_por_compra = document.getElementById("")
+    const total = document.getElementById("")
+
+    const calcular = document.getElementById("");
+    const limpiar = document.getElementById("");
+    //
+	// Methods
+	//
+
+
+
+    //
+	// Inits & Event Listeners
+	//
+})();
