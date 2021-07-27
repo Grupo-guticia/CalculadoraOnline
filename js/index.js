b566ee192a8de9e8f3a63c5073aea80b0e16e231
crisariasgg <crisarias@grupoguticia.com>
2021-07-27 15:51:07 -0600

    added clean method to clean all inputs in calculator

--- js/index.js
+++ js/index.js
@@ -1,30 +1,51 @@
-(function () {
 
-    //
+import Calculator from "./calculator.js";
+
+(function () {
+	//
 	// Variables
 	//
-    const peso = document.getElementById("peso_libras");
-    const valor_compra = document.getElementById("valor_compra");
-    const producto = document.getElementById("producto");
-    const delivery = document.getElementsByTagName("radioDelivery");
+	const peso = document.getElementById("peso_libras");
+	const valor_compra = document.getElementById("valor_compra");
+	const producto = document.getElementById("producto");
+	const delivery = document.getElementsByName("radioDelivery");
 
-    const flete = document.getElementById("flete")
-    const tramite_aduanal = document.getElementById("tramite_aduanal")
-    const impuestos = document.getElementById("impuestos")
-    const cepa = document.getElementById("cepa")
-    const total_cargos_importacion = document.getElementById("total_cargos_importacion")
-    const comision_por_compra = document.getElementById("comision_por_compra")
-    const total = document.getElementById("total")
+	const flete = document.getElementById("flete")
+	const tramite_aduanal = document.getElementById("tramite_aduanal")
+	const impuestos = document.getElementById("impuestos")
+	const cepa = document.getElementById("cepa")
+	const total_cargos_importacion = document.getElementById("total_cargos_importacion")
+	const comision_por_compra = document.getElementById("comision_por_compra")
+	const total = document.getElementById("total")
 
-    const calcular = document.getElementById("calcular");
-    const limpiar = document.getElementById("limpiar");
-    //
+	const calcular = document.getElementById("calcular");
+	const limpiar = document.getElementById("limpiar");
+	
+	//
 	// Methods
 	//
 
 
+	limpiar.onclick = () => calculator.clean();
 
-    //
+
+	//
 	// Inits & Event Listeners
 	//
+
+	const calculator = new Calculator({
+		peso: peso,
+		valor_compra: valor_compra,
+		producto: producto,
+		delivery: delivery,
+		flete: flete,
+		tramite_aduanal: tramite_aduanal,
+		impuestos: impuestos,
+		cepa:cepa,
+		total_cargos_importacion:total_cargos_importacion,
+		comision_por_compra:comision_por_compra,
+		total:total
+		
+	});
+
 })();
